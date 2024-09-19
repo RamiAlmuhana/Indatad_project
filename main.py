8411ac689fa71e3c9433499b522bd0544cc72f0a
Rami Al-Muhana <ramealmuhana@hotmail.com>
2024-09-19 10:41:39 +0200

    python script toegevoegd, docker-compose.yml aangemaakt

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 print("hello world")
-print("hello world")
+
+import psycopg2
+
+
+def insert_now():
+    conn = psycopg2.connect(database='indatad_s1148232',
+                            user='s1148232',
+                            host='95.217.3.61',
+                            password='s1148232',
+                            port=5432)
+
+    cur = conn.cursor()
+
+    insert_query = "INSERT INTO deploy (id, deploy_timestamp) VALUES (DEFAULT, NOW())"
+    cur.execute(insert_query)
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+
+insert_now()
